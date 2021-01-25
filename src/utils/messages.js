665dfb55fe327fb4ad5c1696548e71d0a3bc63c2
ANKITA <62896357+Ankitabit3496@users.noreply.github.com>
2021-01-25 20:20:31 +0530

    Init commit

---
+++ src/utils/messages.js
@@ -0,0 +1,26 @@
+var generateMessage = (username, text) => {
+    return {
+        username,
+        text,
+        createdAt: new Date().getTime()
+    }
+}
+
+//
+// ─── MESSAGE FOR LOCATION ───────────────────────────────────────────────────────
+//
+
+var generateLocationMessage = (username, url) => {
+    return{
+        username, 
+        url,
+        createdAt: new Date().getTime()
+    }
+}
+
+// ────────────────────────────────────────────────────────────────────────────────
+
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+}
